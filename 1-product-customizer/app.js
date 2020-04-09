517d2fd6db693082664df31695b92a0d152105f1
Djan K. Blom <djan@live.dk>
2020-04-09 18:04:37 +0200

    props and ES6

--- 1-product-customizer/app.js
+++ 1-product-customizer/app.js
@@ -1,13 +1,38 @@
 (function () {
   "use strict";
 
+  function SizeSelector(props) {
+    function sizeOptions() {
+      var sizes = window.Inventory.allSizes;
+
+      return sizes.map(function (num) {
+        return (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        );
+      });
+    }
+
+    return (
+      <div className="field-group">
+        <label htmlFor="size-options">Size:</label>
+        <select defaultValue={props.size} name="sizeOptions" id="size-options">
+          {sizeOptions()}
+        </select>
+      </div>
+    );
+  }
+
   function ProductImage(props) {
     // return React.createElement("img", {
     //   src: "./assets/red.jpg",
     //   alt: "Product Image",
     // });
     // Converted to JSX
-    return <img src="./assets/red.jpg" alt="Product Image" />;
+    // return <img src={"./assets/" + props.color + ".jpg"} alt="Product Image" />;
+    // Converted to ES6
+    return <img src={`./assets/${props.color}.jpg`} alt="Product Image" />;
   }
 
   function ProductCustomizer(props) {
@@ -23,7 +48,10 @@
     return (
       <div className="customizer">
         <div className="product-image">
-          <ProductImage />
+          <ProductImage color="green" />
+        </div>
+        <div className="selectors">
+          <SizeSelector size={8} />
         </div>
       </div>
     );
