fb069eb52779d3304736245615e9ff627038fdef
Djan K. Blom <djan@live.dk>
2020-04-10 15:08:31 +0200

    Basic Scaffolding

--- 2-search-people-directory/directory.js
+++ 2-search-people-directory/directory.js
@@ -1,4 +1,24 @@
-(function() {
+(function () {
   "use strict";
 
+  // class component is ES6 syntax
+  class Directory extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      return (
+        <div>
+          <h2>Company Directory</h2>
+          <p>
+            Learn more about each person at Leaf & Mortar in this company
+            directory.
+          </p>
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(<Directory />, document.getElementById("directory-root"));
 })();
