cb7dd02b7e18726d803ef40019158c50a897cec0
Djan K. Blom <djan@live.dk>
2020-04-10 13:01:40 +0200

    selectors added and set up

--- 1-product-customizer/app.js
+++ 1-product-customizer/app.js
@@ -3,9 +3,9 @@
 
   function SizeSelector(props) {
     function sizeOptions() {
-      var sizes = window.Inventory.allSizes;
+      // var sizes = window.Inventory.allSizes;
 
-      return sizes.map(function (num) {
+      return props.sizes.map(function (num) {
         return (
           <option value={num} key={num}>
             {num}
@@ -24,6 +24,27 @@
     );
   }
 
+  function ColorSelector(props) {
+    function colorOptions() {
+      return props.colors.map(function (name) {
+        return (
+          <option value={name} key={name}>
+            {name}
+          </option>
+        );
+      });
+    }
+
+    return (
+      <div className="field-group">
+        <label htmlFor="color-options">Color:</label>
+        <select defaultValue={props.color} name="colorOptions" id="color-options">
+          {colorOptions()}
+        </select>
+      </div>
+    );
+  }
+
   function ProductImage(props) {
     // return React.createElement("img", {
     //   src: "./assets/red.jpg",
@@ -35,27 +56,59 @@
     return <img src={`./assets/${props.color}.jpg`} alt="Product Image" />;
   }
 
-  function ProductCustomizer(props) {
-    // return React.createElement(
-    //   "div",
-    //   { className: "customizer" },
-    //   React.createElement(
-    //     "div",
-    //     { className: "product-image" },
-    //     React.createElement(ProductImage)
-    //   )
-    // );
-    return (
-      <div className="customizer">
-        <div className="product-image">
-          <ProductImage color="green" />
+  // ES5 createClass / React without ES6
+  var ProductCustomizer = createReactClass({
+    // When tracking state, we need to use Classes in React.
+    getInitialState: function () {
+      // getInitialState is for createReactClass. ES6 uses the Constructor.
+
+      var sizes = window.Inventory.allSizes;
+      var colors = window.Inventory.allColors;
+
+      return {
+        color: 'red',
+        colors: colors,
+        size: 8,
+        sizes: sizes
+      }
+    },
+
+    render: function () {
+      return (
+        <div className="customizer">
+          <div className="product-image">
+            <ProductImage color={this.state.color} />
+          </div>
+          <div className="selectors">
+            <SizeSelector size={this.state.size} sizes={this.state.sizes} />
+            <ColorSelector color={this.state.color} colors={this.state.colors} />
+          </div>
         </div>
-        <div className="selectors">
-          <SizeSelector size={8} />
-        </div>
-      </div>
-    );
-  }
+      );
+    }
+  });
+
+  // function ProductCustomizer(props) {
+  //   // return React.createElement(
+  //   //   "div",
+  //   //   { className: "customizer" },
+  //   //   React.createElement(
+  //   //     "div",
+  //   //     { className: "product-image" },
+  //   //     React.createElement(ProductImage)
+  //   //   )
+  //   // );
+  //   return (
+  //     <div className="customizer">
+  //       <div className="product-image">
+  //         <ProductImage color="green" />
+  //       </div>
+  //       <div className="selectors">
+  //         <SizeSelector size={8} />
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   ReactDOM.render(<ProductCustomizer />, document.getElementById("react-root"));
 })();
